3127f17eaec243d53251e2307fbb838158d82fac
Julian Garcia <g.julian32@gmail.com>
2018-11-14 18:05:58 -0600

    Initial Commit

---
+++ ShareMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareMe
+//
+//  Created by Julian Garcia on 11/14/18.
+//  Copyright © 2018 Julian Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
